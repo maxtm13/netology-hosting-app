8fbf508e155ae9c081440710b47d1a5534e84b3c
maxtm13 <maxtm13@gmail.com>
2024-05-07 09:46:15 +0300

    redirect /web/

--- web/index.php
+++ web/index.php
@@ -1,35 +1,34 @@
 <?php
     session_start();
-    ?>
-    <style>
-        body {
-            padding: 5px 20%;
-        }
-    </style>
-    <!doctype html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-    </head>
-    <body>
-    <?php
+?>
+<style>
+    body {
+        padding: 5px 20%;
+    }
+</style>
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
     if (empty($_SESSION['name'])) {
         include 'author.php';
     } else {
         ?>
-        <div class="greeting">Добрый день, <?php echo $_SESSION['name']?>!</div>
+        <div class="greeting">Добрый день, <?php echo $_SESSION['name'] ?>!</div>
         <form action="exit.php" method="post" name="exit">
             <input type="submit" value="Очистить">
         </form>
-
-    <?php
+        <?php
     }
-    ?>
-    </body>
-    </html>
+?>
+</body>
+</html>
 
 
--- web/post.php
+++ web/post.php
@@ -2,3 +2,4 @@
     session_start();
     $_SESSION['name'] = strip_tags($_POST['name']);
     header("Location: /");
+
